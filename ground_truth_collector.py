7f29f9aed087f1f01b2dcd85a06b91a6adb29b93
justin-clarke <36090896+justin-clarke@users.noreply.github.com>
2020-12-10 15:37:19 -0500

    Add files via upload

---
+++ ground_truth_collector.py
@@ -0,0 +1,62 @@
+import sys
+sys.path.append('./src')
+sys.path.append('..')
+from time import sleep
+import json
+from client.agent_client import AgentClient
+
+
+
+if __name__ == "__main__":
+
+    # set up communication
+    default_agent_host = "127.0.0.1"
+    default_agent_port = 2004
+    default_observer_host = "127.0.0.1"
+    default_observer_port = 2006
+    id_num = 28888
+    try:
+        #Wrapper of the communicating messages
+        with open('./src/client/server_client_config.json', 'r') as config:
+            sc_json_config = json.load(config)
+            default_agent_host = sc_json_config[0]["host"]
+            default_agent_port = sc_json_config[0]["port"]
+            agent_ip = default_agent_host
+            agent_port = default_agent_port
+    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+        print("server_client_config.json not found")
+    ar = AgentClient(agent_ip, agent_port)
+    try:
+            ar.connect_to_server()
+    except socket.error as e:
+        logger.error("Error in client-server communication: " + str(e))
+    ar.configure(id_num)
+    ar.ready_for_new_set()
+    ar.ready_for_new_set()
+
+
+    ground_truths = []
+    noisy_ground_truths = []
+
+    level = 0
+    while level < 200:
+        if level % 25 == 0:
+            print("Loading level: " + str(level))
+        ar.load_next_available_level()
+        sleep(1.75)
+        ground_truths.append(ar.get_ground_truth_without_screenshot()[0])
+        for sample in range(100):
+            noisy_ground_truths.append(ar.get_noisy_ground_truth_without_screenshot()[0])
+        level += 1
+
+    
+    with open("novelty37_200_noisy_groundtruths_100samples_v0411.txt", 'w') as f:
+        json.dump(noisy_ground_truths, f)
+
+    with open("novelty37_200_non-noisy_groundtruths_v0411.txt", 'w') as f2:
+        json.dump(ground_truths, f2)
+
+
+
+
+
