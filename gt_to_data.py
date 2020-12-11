54d91ac51b3092e84ee7ea860369c63a5508650d
justin-clarke <36090896+justin-clarke@users.noreply.github.com>
2020-12-11 01:53:39 -0500

    Update gt_to_data.py

--- gt_to_data.py
+++ gt_to_data.py
@@ -2,7 +2,7 @@
 import json
 from gt_update import *
 from get_data import *
-f = open("non-novel_50_noisy_groundtruths_100samples_v0411.txt")
+f = open("non-novel_noisy_groundtruths_0-80_100samples_v041_aws.txt")
 gt_list = json.load(f)
 data = []
 for gt in gt_list:
@@ -11,7 +11,7 @@ for gt in gt_list:
 	data.extend(d)
 
 
-with open("non-novel_50levels_100samples_v0411.txt", 'w') as f:
+with open("non-novel_0-80_100samples_v041_aws.txt", 'w') as f:
         for i in data:
         	f.write(str(i) + '\n')
 
